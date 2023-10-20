360b032c813b07d8fa3ad7ad36d788cfb4cc7c4f
trekintech <45718432+trekintech@users.noreply.github.com>
2023-10-20 21:51:01 +0100

    Update sizer.ps1
    Updated to write out and display on screen

--- DMaaS/AWS/Sizing/sizer.ps1
+++ DMaaS/AWS/Sizing/sizer.ps1
@@ -1,81 +1,50 @@
-# Check for AWSPowerShell module and install if not present
-if (-Not (Get-Module -ListAvailable -Name AWSPowerShell)) {
-    "AWSPowerShell module not found. Installing now..." | Out-File -Append "Cohesity_AWS_Sizing.txt"
-    Install-Module -Name AWSPowerShell -Scope CurrentUser -Force -SkipPublisherCheck
+# Load AWS Module
+Import-Module AWS.Tools.Common -ErrorAction Stop
+
+# Prompt for AWS Credentials if not set
+if (-Not (Test-Path $env:USERPROFILE\.aws\credentials)) {
+    Initialize-AWSDefaultConfiguration -ProfileName default
 }
 
-# Import AWS module
-Import-Module AWSPowerShell
+# Define regions
+$regions = @("us-east-1", "us-east-2", "us-west-1", "us-west-2")
 
-# Prompt for AWS Credentials if not already set
-if (-Not (Get-AWSCredential -ListProfileDetail)) {
-    $accessKey = Read-Host -Prompt "Enter your AWS Access Key"
-    $secretKey = Read-Host -Prompt "Enter your AWS Secret Key" -AsSecureString
-    $region = Read-Host -Prompt "Enter your default AWS region (e.g., us-east-1)"
-    Set-AWSCredential -AccessKey $accessKey -SecretKey $secretKey -StoreAs default
-    Initialize-AWSDefaults -ProfileName default -Region $region
+# Initialize output file
+$outputFile = "Cohesity_AWS_Sizing.txt"
+if (Test-Path $outputFile) {
+    Remove-Item $outputFile
 }
 
-# List of all AWS regions you want to check
-$regions = @( 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1' )  # Add more regions as needed
-
+# Loop through each AWS region
 foreach ($region in $regions) {
-    "-------- Region: ${region} --------" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-
-    # Initialize variables for S3
-    $allBucketsSize = 0
-
-    # Get list of all S3 Buckets in the current region
-    $buckets = Get-S3Bucket | Where-Object { $_.Location.Value -eq $region }
-    "Total number of S3 Buckets in ${region}: $($buckets.Count)" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-
-    # EC2 logic
+    "Processing region: $region" | Tee-Object -Append -FilePath $outputFile
+    
+    # Fetch S3 Buckets
+    $buckets = Get-S3Bucket
+    "Total number of S3 Buckets in region $region: $($buckets.Count)" | Tee-Object -Append -FilePath $outputFile
+    
+    # Fetch EC2 Instances
     $ec2Instances = Get-EC2Instance -Region $region
-    $runningInstanceIds = $ec2Instances | Where-Object { $_.Instances[0].State.Name -eq 'running' } | ForEach-Object { $_.Instances[0].InstanceId }
-    $nonRunningInstanceIds = $ec2Instances | Where-Object { $_.Instances[0].State.Name -ne 'running' } | ForEach-Object { $_.Instances[0].InstanceId }
-
-    "Total number of EC2 Instances in ${region}: $($ec2Instances.Count)" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-    "Number of running EC2 Instances in ${region}: $($runningInstanceIds.Count)" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-    "Number of non-running EC2 Instances in ${region}: $($nonRunningInstanceIds.Count)" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-
-    # EBS Logic
+    $runningEc2Instances = $ec2Instances | Where-Object {$_.Instances.State.Name -eq 'running'}
+    $stoppedEc2Instances = $ec2Instances | Where-Object {$_.Instances.State.Name -ne 'running'}
+    "Total number of running EC2 Instances in region $region: $($runningEc2Instances.Count)" | Tee-Object -Append -FilePath $outputFile
+    "Total number of stopped EC2 Instances in region $region: $($stoppedEc2Instances.Count)" | Tee-Object -Append -FilePath $outputFile
+    
+    # Fetch EBS Volumes
     $ebsVolumes = Get-EC2Volume -Region $region
-    $ebsRunningTotalSize = 0
-    $ebsNonRunningTotalSize = 0
-
-    foreach ($volume in $ebsVolumes) {
-        if ($runningInstanceIds -contains $volume.Attachment.InstanceId) {
-            $ebsRunningTotalSize += $volume.Size
-        } elseif ($nonRunningInstanceIds -contains $volume.Attachment.InstanceId) {
-            $ebsNonRunningTotalSize += $volume.Size
-        }
-    }
-
-    $ebsRunningTotalSizeTB = [math]::Round(($ebsRunningTotalSize / 1024), 2)
-    $ebsNonRunningTotalSizeTB = [math]::Round(($ebsNonRunningTotalSize / 1024), 2)
-
-    "Total size of EBS volumes attached to running instances in ${region}: $($ebsRunningTotalSizeTB) TB" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-    "Total size of EBS volumes attached to non-running instances in ${region}: $($ebsNonRunningTotalSizeTB) TB" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-
-    # RDS Logic
+    $runningEbsTotalSize = ($ebsVolumes | Where-Object {$_.Attachments.State -eq 'attached'}).Size | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $stoppedEbsTotalSize = ($ebsVolumes | Where-Object {$_.Attachments.State -ne 'attached'}).Size | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    "Total size of attached EBS volumes in region $region: {0:N2} GB" -f ($runningEbsTotalSize / 1024) | Tee-Object -Append -FilePath $outputFile
+    "Total size of detached EBS volumes in region $region: {0:N2} GB" -f ($stoppedEbsTotalSize / 1024) | Tee-Object -Append -FilePath $outputFile
+    
+    # Fetch RDS Instances
     $rdsInstances = Get-RDSDBInstance -Region $region
-    $rdsEngineTypes = @{}
-    $rdsTotalSize = 0
-    foreach ($instance in $rdsInstances) {
-        $engine = $instance.Engine
-        $size = $instance.AllocatedStorage
-        $rdsTotalSize += $size
-        if ($rdsEngineTypes.ContainsKey($engine)) {
-            $rdsEngineTypes[$engine] += $size
-        } else {
-            $rdsEngineTypes[$engine] = $size
-        }
-    }
-    $rdsTotalSizeTB = [math]::Round(($rdsTotalSize / 1024), 2)
-    "Total size of RDS Instances in ${region}: $($rdsTotalSizeTB) TB" | Out-File -Append "Cohesity_AWS_Sizing.txt"
-
-    foreach ($key in $rdsEngineTypes.Keys) {
-        $engineSizeTB = [math]::Round(($rdsEngineTypes[$key] / 1024), 2)
-        "RDS Engine type $key in ${region} total size: $($engineSizeTB) TB" | Out-File -Append "Cohesity_AWS_Sizing.txt"
+    $groupedRds = $rdsInstances | Group-Object -Property DBInstanceClass
+    "Total number of RDS Instances in region $region: $($rdsInstances.Count)" | Tee-Object -Append -FilePath $outputFile
+    foreach ($group in $groupedRds) {
+        $totalSize = ($group.Group | Measure-Object -Property AllocatedStorage -Sum).Sum
+        "RDS Instance Type: $($group.Name), Count: $($group.Count), Total size: {0:N2} GB" -f ($totalSize / 1024) | Tee-Object -Append -FilePath $outputFile
     }
 }
+
+"Output saved to file: $((Resolve-Path $outputFile).Path)" | Tee-Object -Append -FilePath $outputFile
